78cbbd7896f7f92c5751c27b390085cf3137919a
Arion Almond <aalmond@avidxchange.com>
2018-02-07 14:34:20 -0500

    added card controller and view

---
+++ MVC/Controllers/CardController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class CardController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public string Welcome()
+        //{
+        //    return "This is the Welcome action method...";
+        //}
+
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+        //}
+
+        //public string Welcome(string name, int id = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + id);
+        //}
+
+
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello" + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
